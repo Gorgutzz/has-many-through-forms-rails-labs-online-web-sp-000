e6fea370a3232006c6d524031da5d4489c4fa22b
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-02 01:53:25 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,11 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: :username
 
-end
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      if !user_attribute.empty?
+        user = User.find_or_create_by(username: user_attribute)
+        self.user = user
+      end
+    end
+  end
